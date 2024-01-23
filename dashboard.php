17e1dcaa435cb6f8a4d06afbb3035f71a2155925
stappost <emanuele.scarpantoni@gmail.com>
2024-01-23 15:28:58 +0100

    [END PROJECT] add text in dashboard

--- dashboard.php
+++ dashboard.php
@@ -1,3 +1,8 @@
+<?php
+    $text= $_GET["text"];
+    $word= $_GET["word"];
+    $new_text= str_replace($word, '***', $text)
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +11,21 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <h2>Text with badwords</h2>
+    <p>
+        <?php echo $text ?>
+        <div>
+            La lunghezza del paragrafo è:
+            <?php echo strlen($text) ?>
+        </div>
+    </p>
+    <h2>Text without badwords</h2>
+    <p>
+        <?php echo $new_text ?>
+        <div>
+            La lunghezza del nuovo paragrafo è:
+            <?php echo strlen($new_text) ?>
+        </div>
+    </p>
 </body>
 </html>
